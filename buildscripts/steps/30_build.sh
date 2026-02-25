dadb354d5c56542b5f7844991ce4a22b03a14d2c
kotborealis <kotborealis@awooo.ru>
2026-02-25 13:26:29 +0000

    Increase build heap for VS Code server

--- buildscripts/steps/30_build.sh
+++ buildscripts/steps/30_build.sh
@@ -3,5 +3,4 @@
 set -ex
 
 # Compile
-export VSCODE_NO_SOURCEMAP=1
-node --max-old-space-size=3072 ./node_modules/gulp/bin/gulp.js vscode-reh-web-linux-x64
+node --max-old-space-size=6144 ./node_modules/gulp/bin/gulp.js vscode-reh-web-linux-x64
